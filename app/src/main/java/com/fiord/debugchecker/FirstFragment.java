afc8f6df9edd5206c91f7806ab98b0773d6fb74f
fiord <hyoga_quasar@yahoo.co.jp>
2023-06-15 11:34:09 +0900

    version 2.0

--- app/src/main/java/com/fiord/debugchecker/FirstFragment.java
+++ app/src/main/java/com/fiord/debugchecker/FirstFragment.java
@@ -1,6 +1,7 @@
 package com.fiord.debugchecker;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -17,6 +18,11 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.fiord.debugchecker.databinding.FragmentFirstBinding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class FirstFragment extends Fragment {
 
@@ -58,7 +64,7 @@ public class FirstFragment extends Fragment {
                 TextView adb_enabled = (TextView) binding.getRoot().getViewById(R.id.adb_enabled);
                 adb_enabled.setCompoundDrawablesWithIntrinsicBounds(0, 0, DebugResultIcon(adb),0);
 
-                // with wifi detection
+                // with wifi detection. I think it won't work...
                 int wifi = Settings.Secure.getInt(context.getContentResolver(), "adb_wifi_enabled", 0);
                 adb_enabled = (TextView) binding.getRoot().getViewById(R.id.adb_wifi_enabled);
                 adb_enabled.setCompoundDrawablesWithIntrinsicBounds(0, 0, DebugResultIcon(wifi),0);
@@ -67,6 +73,25 @@ public class FirstFragment extends Fragment {
                 int developer_mode = Settings.Secure.getInt(context.getContentResolver(), Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0);
                 adb_enabled = (TextView) binding.getRoot().getViewById(R.id.developer_mode_enabled);
                 adb_enabled.setCompoundDrawablesWithIntrinsicBounds(0, 0, DebugResultIcon(developer_mode), 0);
+
+                // get all processes. but it is impossible to detect adbd, because of application userspace.
+//                try {
+//                    ProcessBuilder processBuilder = new ProcessBuilder("ps", "-A");
+//                    Process process = processBuilder.start();
+//                    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+//                    String line;
+//                    while ((line = reader.readLine()) != null) {
+//                        Log.d("Running Processes", "()()" + line);
+//                    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//                ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+//                List<ActivityManager.RunningServiceInfo> runningServices = manager.getRunningServices(Integer.MAX_VALUE);
+//                Log.d("Activity Processes", "num:" + runningServices.size());
+//                for (ActivityManager.RunningServiceInfo service : runningServices) {
+//                    Log.d("Activity Processes", "()" + service.service.getClassName());
+//                }
             }
         });
     }
